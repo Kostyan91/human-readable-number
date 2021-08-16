926f9dc14c6e6ec480f8b6a340c3928aa5761e7f
Kostyan91 <kostyan912404@gmail.com>
2021-08-16 18:42:18 +0400

    fead add solution

--- src/index.js
+++ src/index.js
@@ -20,17 +20,12 @@ module.exports = function toReadable (number) {
             if(number==dozensNumder[i])
           return (dozensWords[i]);
           }
-          for (let i = 0; i <= unitsNumber.length; i++) {
-            if (number / 10 == dozensNumder[i] && number % 100 !== 0) 
-            return (`${dozensWords[i]} hundred`);
-          }
-          /// 100 -900
-          if(number % 100 == 0){
+           ///100 -900
             for (let i = 0; i <= unitsNumber.length; i++) {
-                if (number  == unitsNumber[i]) 
-                return (`${unitsWords[i]} 'hundred`);
+                if (number / 100  == unitsNumber[i]) 
+                return (`${unitsWords[i]} hundred`);
               }
-        }
+        ////21-99
         if ((number / 10) && number / 10 > 1 && number / 10 < 10) {
           let units = number % 10;
           let dozens = Math.trunc(number / 10);
@@ -66,8 +61,6 @@ module.exports = function toReadable (number) {
         dozens = dozensWords[i];
       if (dozens == 0) dozens = "";
     }
-
-    
     return (`${hundreds}` + " hundred " + `${dozens} ${units}`);
   }
 /////////// 101,605
@@ -80,7 +73,6 @@ module.exports = function toReadable (number) {
     let hundreds = Math.trunc(number / 100);
     let units = number % 10;
    
-
     for (let i = 0; i <= unitsNumber.length; i++) {
       if (hundreds == unitsNumber[i]) hundreds = unitsWords[i];
     }
@@ -91,8 +83,7 @@ module.exports = function toReadable (number) {
     return (`${hundreds}` + " hundred " + `${units}`);
   }
   ////////// 916 
-  if (
-    !Number.isInteger(number / 10) && number % 100 >= 10 && number % 100 <= 19
+  if (!Number.isInteger(number / 10) && number % 100 >= 10 && number % 100 <= 19
   ) {
     let hundreds = Math.trunc(number / 100);
     let units = number % 100;
@@ -104,25 +95,20 @@ module.exports = function toReadable (number) {
     }
     return (`${hundreds}` + " hundred " + `${units}`);
   }
-
-  if (Number.isInteger(number / 10)) {
+///930
     let hundreds = Math.trunc(number / 100);
-
     let dozens = (number % 100) ;
+
     for (let i = 0; i <= unitsNumber.length; i++) {
       if (hundreds == unitsNumber[i]) hundreds = unitsWords[i];
     }
-
     for (let i = 0; i <= unitsNumber.length; i++) {
-      if (dozens == dozensNumder[i]) dozens = dozensWords[i]
-   
+      if (dozens == dozensNumder[i]) dozens = dozensWords[i] 
 }
-    
-    return(`${hundreds}` + " hundred " + `${dozens}`);
-  
+    return(`${hundreds}` + ' hundred ' + `${dozens}`);
 }
-        }
+        
       
-        toReadable(`${['1']}`)
+       // toReadable(`${['1']}`)
         
 
