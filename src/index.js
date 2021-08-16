990f1ce641e4ee2a15efe946841db3be4e649bff
Kostyan91 <kostyan912404@gmail.com>
2021-08-16 17:28:40 +0400

    fead add soluttion

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 module.exports = function toReadable (number) {
-    function toReadable(number) {
         let unitsNumber = [
           0,1,2,3, 4,5,6,7,8,9,10,11,12,13,14,15, 16, 17,18,19,
         ];
@@ -11,39 +10,119 @@ module.exports = function toReadable (number) {
           let dozensWords = [
               "ten","twenty", "thirty","forty","fifty", "sixty", "seventy", "eighty", "ninety",
           ];
-      
+      ///// 1-19
           for(let i = 0; i<=unitsNumber.length; i++) {
             if(number==unitsNumber[i])
             return (unitsWords[i]);
           }
-          
+          ///10 -90
           for(let i = 0; i<=dozensNumder.length; i++) {
             if(number==dozensNumder[i])
           return (dozensWords[i]);
           }
           for (let i = 0; i <= unitsNumber.length; i++) {
             if (number / 10 == dozensNumder[i] && number % 100 !== 0) 
-            console.log (`${dozensWords[i]} hundred`);
+            return (`${dozensWords[i]} hundred`);
           }
+          /// 100 -900
           if(number % 100 == 0){
             for (let i = 0; i <= unitsNumber.length; i++) {
-                if (number / 100 == unitsNumber[i]) 
-                console.log  (unitsWords[i], 'hundred')
+                if (number  == unitsNumber[i]) 
+                return (`${unitsWords[i]} 'hundred`);
               }
         }
         if ((number / 10) && number / 10 > 1 && number / 10 < 10) {
           let units = number % 10;
-          let dozents = Math.trunc(number / 10);
+          let dozens = Math.trunc(number / 10);
           for (let i = 0; i <= unitsNumber.length; i++) {
             if (units == unitsNumber[i]) units = unitsWords[i];
           }
           for (let i = 0; i <= dozensNumder.length; i++) {
-            if (dozents * 10 == dozensNumder[i]) dozents = dozensWords[i];
+            if (dozens * 10 == dozensNumder[i]) dozens = dozensWords[i];
           }
-          console.log(`${dozents} ${units}`);
+          return(`${dozens} ${units}`);
         }
+         /////// 654,675
+  if (
+    !Number.isInteger(number / 10) &&
+    number / 10 > 10 &&
+    number / 10 < 100 &&
+    number % 100 > 19
+  ) {
+    let hundreds = Math.trunc(number / 100);
+    let dozens = Math.trunc(number / 10) % 10;
+    let units = number % 10;
+    
+    for (let i = 0; i <= unitsNumber.length; i++) {
+      if (hundreds == unitsNumber[i]) hundreds = unitsWords[i];
+    }
+
+    for (let i = 0; i <= unitsNumber.length; i++) {
+      if (units == unitsNumber[i]) units = unitsWords[i];
+    }
+
+    for (let i = 0; i <= dozensNumder.length; i++) {
+      if (dozens * 10 == dozensNumder[i] && dozens !== 0)
+        dozens = dozensWords[i];
+      if (dozens == 0) dozens = "";
+    }
+
+    
+    return (`${hundreds}` + " hundred " + `${dozens} ${units}`);
+  }
+/////////// 101,605
+  if (
+    !Number.isInteger(number / 10) &&
+    number / 10 > 10 &&
+    number / 10 < 100 &&
+    number % 100 < 10
+  ) {
+    let hundreds = Math.trunc(number / 100);
+    let units = number % 10;
+   
+
+    for (let i = 0; i <= unitsNumber.length; i++) {
+      if (hundreds == unitsNumber[i]) hundreds = unitsWords[i];
+    }
+
+    for (let i = 0; i <= unitsNumber.length; i++) {
+      if (units == unitsNumber[i]) units = unitsWords[i];
+    }
+    return (`${hundreds}` + " hundred " + `${units}`);
+  }
+  ////////// 916 
+  if (
+    !Number.isInteger(number / 10) && number % 100 >= 10 && number % 100 <= 19
+  ) {
+    let hundreds = Math.trunc(number / 100);
+    let units = number % 100;
+    for (let i = 0; i <= unitsNumber.length; i++) {
+      if (hundreds == unitsNumber[i]) hundreds = unitsWords[i];
+    }
+    for (let i = 0; i <= unitsNumber.length; i++) {
+      if (units == unitsNumber[i]) units = unitsWords[i];
+    }
+    return (`${hundreds}` + " hundred " + `${units}`);
+  }
+
+  if (Number.isInteger(number / 10)) {
+    let hundreds = Math.trunc(number / 100);
+
+    let dozens = (number % 100) ;
+    for (let i = 0; i <= unitsNumber.length; i++) {
+      if (hundreds == unitsNumber[i]) hundreds = unitsWords[i];
+    }
+
+    for (let i = 0; i <= unitsNumber.length; i++) {
+      if (dozens == dozensNumder[i]) dozens = dozensWords[i]
+   
+}
+    
+    return(`${hundreds}` + " hundred " + `${dozens}`);
+  
+}
         }
       
-        toReadable(`${['2000']}`)
+        toReadable(`${['1']}`)
         
-}
+
